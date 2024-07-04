3cee8383319e7c5d8462e14b119fd3b5f916ba0a
Antti Peuhkurinen <peuhkura@gmail.com>
2024-07-04 20:59:51 +0300

    Showing diagnosis

--- src/components/ShowPatientModal/ShowPatient.tsx
+++ src/components/ShowPatientModal/ShowPatient.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, SyntheticEvent } from "react";
-import { Typography, TextField, InputLabel, MenuItem, Select, Grid, Button, SelectChangeEvent } from '@mui/material';
-import { PatientFormValues, Patient, Gender } from "../../types";
+import { Container, List, ListItem, ListItemText, Divider, Typography, Box, Grid, Button } from '@mui/material';
+import { PatientFormValues, Patient, Gender, Discharge, Entry, EntriesData, Diagnosis } from "../../types";
 import { apiBaseUrl } from "../../constants";
+import React from 'react';
 
 interface Props {
   patientId: string;
@@ -22,18 +23,51 @@ const ShowPatient = ({ patientId="{patientId}", onCancel, onSubmit }: Props) => 
   const [name, setName] = useState('');
   const [occupation, setOccupation] = useState('');
   const [ssn, setSsn] = useState<string>(null);
-  const [dateOfBirth, setDateOfBirth] = useState<string | null>(null);
+  const [dateOfBirth, setDateOfBirth] = useState('');
   const [gender, setGender] = useState(Gender.Other);
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<Patient | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const initialDiagnoseCodes: string[] = [''];
+  const [diagnoseCodes, setDiagnoseCodes] = useState(initialDiagnoseCodes);
+  const initialEntries: Entry[] = [];
+  const [entriesData, setEntriesData] = useState(initialEntries);
+
+  // For test
+  /*const diagnosisCodes: string[] = ['Z57.1', 'Z74.3', 'M51.2'];
+  const diagnoses2: Diagnosis[] = [
+    { code: "M24.2", name: "Disorder of ligament", latin: "Morbositas ligamenti" },
+    { code: "M51.2", name: "Other specified intervertebral disc displacement", latin: "Alia dislocatio disci intervertebralis specificata" },
+    { code: "S03.5", name: "Sprain and strain of joints and ligaments of other and unspecified parts of head", latin: "Distorsio et/sive distensio articulationum et/sive ligamentorum partium aliarum sive non specificatarum capitis" },
+  ];*/
+  //let diagnoses: Diagnosis[];
+
+
+  let initialDiagnosis: Diagnosis[];
+  const [diagnosis, setDiagnosis] = useState(initialDiagnosis);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDiagnoses  = async () => {
       try {
         setLoading(true);
-        //const response = await fetch(`${apiBaseUrl}/patients`);
-        //const response = await fetch(`${apiBaseUrl}/patients?patientId=${patientId}`);
+        const response = await fetch(`${apiBaseUrl}/diagnoses`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: Diagnosis[] = await response.json();
+
+        console.log(JSON.stringify(data));
+        setDiagnosis(JSON.parse(JSON.stringify(data)));
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchPatientData = async () => {
+      try {
+        setLoading(true);
         const response = await fetch(`${apiBaseUrl}/patients?patientId=${encodeURIComponent(patientId)}`);
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -45,29 +79,92 @@ const ShowPatient = ({ patientId="{patientId}", onCancel, onSubmit }: Props) => 
         setOccupation(data[0].occupation);
         setGender(data[0].gender);
         setData(data[0]);
+
         console.log("show patient useEffect.");
+
+        // Use the function to parse JSON data
+        setEntriesData(JSON.parse(JSON.stringify(data[0].entries)));
+        console.log("XXX patient entries:");
+        console.log(entriesData);
       } catch (error: any) {
         setError(error.message);
       } finally {
         setLoading(false);
       }
     };
-
-    fetchData();
+    
+    fetchDiagnoses();
+    fetchPatientData();
   }, [patientId]);
 
-  const onGenderChange = (event: SelectChangeEvent<string>) => {
-    event.preventDefault();
-    if ( typeof event.target.value === "string") {
-      const value = event.target.value;
-      const gender = Object.values(Gender).find(g => g.toString() === value);
-      if (gender) {
-        setGender(gender);
-      }
-    }
+  interface DiagnosisListProps {
+    codes: string[];
+    diagnoses: Diagnosis[];
+  }
+
+  const DiagnosisList: React.FC<DiagnosisListProps> = ({ codes, diagnoses }) => {
+    return (
+      <Container>
+        <br/>
+        <Typography variant="body2"><strong>Diagnosis List:</strong></Typography>
+        <List>
+          {codes.map(code => {
+            const matchingDiagnosis = diagnoses.find(diagnosis => diagnosis.code === code);
+            return (
+                <ListItem alignItems="flex-start" sx={{ padding: '4px 0' }}>
+                  {matchingDiagnosis ? (
+                    <ListItemText variant="body2" sx={{ display: 'block' }} 
+                      primary={`Code: ${matchingDiagnosis.code}, ${matchingDiagnosis.name}`}
+                    />
+                  ) : (
+                    <ListItemText primary={`No matching diagnosis found for code: ${code}`} />
+                  )}
+                </ListItem>
+            );
+          })}
+        </List>
+      </Container>
+    );
   };
 
-  // <pre>{JSON.stringify(data, null, 2)}</pre>
+//   const Entry = ({ entry, diagnosisCodes, diagnoses }) => (
+  interface EntryProps {
+    entry: Entry;
+    diagnosisCodes: string[];
+    diagnoses: Diagnosis[];
+  }
+
+  const Entry: React.FC<EntryProps> =({ entry, diagnosisCodes, diagnoses }) => (
+
+    <div className="entry">
+
+    <Box p={1}>
+      <Typography variant="body2"><strong>Date: {entry.date}</strong></Typography>
+      <Typography variant="body2">Specialist: {entry.specialist}</Typography>
+      <Typography variant="body2">Description: {entry.description}</Typography>
+      {entry.healthCheckRating !== undefined && (
+        <Typography variant="body2">Health Check Rating: {entry.healthCheckRating}</Typography>
+      )}
+      {entry.employerName && (
+        <Typography variant="body2">Employer Name: {entry.employerName}</Typography>
+      )}
+      {entry.discharge && (
+        <Typography variant="body2">Discharge: {entry.discharge.date}, {entry.discharge.criteria}</Typography>
+      )}
+      {diagnosisCodes && (<DiagnosisList codes={diagnosisCodes} diagnoses={diagnoses} />)}
+    </Box>
+
+    </div>
+  );
+
+  /* Snippets
+      {diagnosisCodes && (
+        <Typography variant="body2">Diagnose codes: {diagnosisCodes}</Typography>
+      )}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>MORO:{JSON.stringify(entriesData, null, 2)}</pre>
+      <pre>{JSON.stringify(diagnosisData, null, 2)}</pre>
+    */
 
   return (
     <div>
@@ -76,6 +173,12 @@ const ShowPatient = ({ patientId="{patientId}", onCancel, onSubmit }: Props) => 
       <Typography variant="body1">Date of birth: {dateOfBirth}</Typography>
       <Typography variant="body1">Occupation: {occupation}</Typography>
       <Typography variant="body1">Gender: {gender}</Typography>
+      <Divider></Divider>
+
+      <Typography variant="body1"><strong>Entries:</strong></Typography>
+      {entriesData.map(entry => (
+        <Entry key={entry.id} entry={entry} diagnosisCodes={entry.diagnosisCodes} diagnoses={diagnosis} />
+      ))}
 
       <Grid>
         <Grid item>
@@ -89,8 +192,10 @@ const ShowPatient = ({ patientId="{patientId}", onCancel, onSubmit }: Props) => 
             Close
           </Button>
         </Grid>
-        
       </Grid>
+
+
+
     </div>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,49 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: EntriesData[];
+}
+
+export interface BaseEntry {
+  id: string;
+  type: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+export interface HospitalEntry extends BaseEntry {
+  diagnosisCodes: string[];
+  discharge?: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// Define the interface for the entire JSON structure
+export interface EntriesData {
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
